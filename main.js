b8a05314570c3e6c91e9fe7533547f4159c5c43e
Devin W Bowler <83326036+devinbowler@users.noreply.github.com>
2022-10-21 20:15:46 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -26,13 +26,13 @@ const inViewport = (entries, observer) => {
       for (const classLook of classes) {
         if (classLook.matches('.footer.is-inViewport') && x.matches) {
            youtube.style.transition="all 2s";
-           youtube.style.transform="translateX(175px)" + "translateY(50px)" + "scale(2, 2)";
+           youtube.style.transform="translateX(175px)" + "translateY(75px)" + "scale(2, 2)";
            youtube.style.color = "beige";
            linkedin.style.transition="all 2s";
-           linkedin.style.transform="translateX(125px)" + "scale(2, 2)";
+           linkedin.style.transform="translateX(125px)" + "translateY(25px)" + "scale(2, 2)";
            linkedin.style.color = "beige";
            github.style.transition="all 2s";
-           github.style.transform="translateX(225px)" + "translateY(100px)" + "scale(2, 2)";
+           github.style.transform="translateX(225px)" + "translateY(125px)" + "scale(2, 2)";
            github.style.color = "beige";
            line.style.transition="all 2s";
            line.style.transform="translateY(100%)";
