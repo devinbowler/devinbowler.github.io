dc9e0882f3a0d6bb87962c998266b7995607b2b5
Devin W Bowler <devinbowler@gmail.com>
2024-11-29 03:00:18 -0500

    Blog updates.

--- components/BlogPage/BlogPage.js
+++ components/BlogPage/BlogPage.js
@@ -25,11 +25,11 @@ export class BlogPage extends BaseComponent {
   #setupContainerContent() {
     this.#container.innerHTML = `
       <div class="blog-post" data-post="blog1">
-        <h2>Finding Meaning</h2>
+        <h2>Blog Post Coming Soon</h2>
         <p class="date">Published: November 28, 2024</p>
         <hr class="separator" />
         <p class="summary">
-          In this post I wanted to write down some thoughts I had on my view of meaning in life, what if could be and what I draw from as a sort of ispiration to keep going about my life. I think the topic and answer is different for every person, and perhaps it is better that way, or maybe a difference on meaning is what seprates us.
+           A summary of the blog will be posted here.
         </p>
       </div>
     `;
